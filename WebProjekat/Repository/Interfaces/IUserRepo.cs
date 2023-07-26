87b80652c37b9036e7ec4e9354be48395fbf6b8f
mmitrovic00 <101202217+mmitrovic00@users.noreply.github.com>
2023-07-26 12:39:00 +0200

    Dodato mapiranje i repozitorijum

---
+++ WebProjekat/Repository/Interfaces/IUserRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProjekat.Repository.Interfaces
+{
+	interface IUserRepoe
+	{
+	}
+}
